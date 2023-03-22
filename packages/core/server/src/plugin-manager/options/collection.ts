59f7d187dea37f95e893dc03d2a1d0b01c2f770b
yingying <yingying.xwy@antgroup.com>
2023-03-22 15:13:25 +0800

    chore: 插件表增加默认字段

--- packages/core/server/src/plugin-manager/options/collection.ts
+++ packages/core/server/src/plugin-manager/options/collection.ts
@@ -6,7 +6,9 @@ export default {
     { type: 'string', name: 'version' },
     { type: 'boolean', name: 'enabled' },
     { type: 'boolean', name: 'installed' },
+    { type: 'boolean', name: 'used' },
     { type: 'boolean', name: 'builtIn' },
+    { type: 'string', name: 'path' },
     { type: 'json', name: 'options' },
   ],
 };
